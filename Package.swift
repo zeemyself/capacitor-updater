0f774aeef71491ae7fc606da7d4571ff665d3e9a
zeemyself <zeemyself@gmail.com>
2024-09-20 14:23:46 +0700

    Fix spm Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
@@ -9,14 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "CapgoCapacitorUpdater",
-            targets: ["Plugin"]
+            targets: ["CapgoCapacitorUpdater"]
         )
     ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "main"),
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(url: "https://github.com/mrackwitz/Version.git", from: "1.0.0")
+        .package(url: "https://github.com/mrackwitz/Version.git", from: "0.0.0")
     ],
     targets: [
         .target(
